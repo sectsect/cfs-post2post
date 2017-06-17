571d27a7674077e49a6c816d2b08faf58d3b7ec6
SECT <info@sectwebstudio.com>
2017-06-18 02:07:00 +0900

    Change the page-type for setting to the sub menu page

--- functions/admin.php
+++ functions/admin.php
@@ -1,15 +1,33 @@
 <?php
 /*==================================================
-    Add Options Page
+    Add Sub Menu Page
 ================================================== */
 add_action('admin_menu', 'cfs_p2p_menu');
 function cfs_p2p_menu()
 {
-    $page_hook_suffix = add_options_page('CFS Post 2 Post', 'CFS Post 2 Post', 8, 'cfs_p2p_menu', 'cfs_p2p_options_page');
+    // $page_hook_suffix = add_options_page('CFS Post 2 Post', 'CFS Post 2 Post', 8, 'cfs_p2p_menu', 'cfs_p2p_options_page');
+    if ( false === apply_filters( 'cfs_disable_admin', false ) ) {
+        $page_hook_suffix = add_submenu_page( 'edit.php?post_type=cfs', 'CFS Post 2 Post', 'CFS Post 2 Post', 'manage_options', 'cfs_p2p_menu', 'cfs_p2p_options_page' );
+    }
     add_action('admin_print_styles-' . $page_hook_suffix, 'cfs_p2p_admin_styles');
     add_action('admin_print_scripts-' . $page_hook_suffix, 'cfs_p2p_admin_scripts');
     add_action('admin_init', 'register_cfs_p2p_settings');
 }
+
+/**
+ * admin_menu
+ */
+function admin_menu() {
+    if ( false === apply_filters( 'cfs_disable_admin', false ) ) {
+        add_submenu_page( 'edit.php?post_type=cfs', __( 'Tools', 'cfs' ), __( 'Tools', 'cfs' ), 'manage_options', 'cfs-tools', array( $this, 'page_tools' ) );
+    }
+
+    // Remove the "Add New" menu link
+    global $submenu;
+    unset( $submenu['edit.php?post_type=cfs'][10] );
+}
+
+
 function cfs_p2p_admin_styles()
 {
     wp_enqueue_style('select2', '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css', array());
@@ -28,6 +46,33 @@ function cfs_p2p_options_page()
 {
     require_once plugin_dir_path(dirname(__FILE__)) . 'admin/index.php';
 }
+
+/**
+ * Order the sub-menu
+ */
+function cfs_p2p_submenu_order( $menu_ord )
+{
+    global $submenu;
+
+	foreach ($submenu as $key => $menus) {
+		if ("edit.php?post_type=cfs" === $key) {
+			// var_dump($menus);
+			foreach ($menus as $subkey => $menu) {
+				if (11 === $subkey) {
+					$cfs_post2post_menu = array($subkey => $menu);
+				}
+			}
+			unset($menus[11]);
+			$menus += $cfs_post2post_menu;
+			$orderedmenu = $menus;
+		}
+	}
+	$submenu['edit.php?post_type=cfs'] = $orderedmenu;
+	// var_dump($submenu);
+
+    return $submenu;
+}
+add_filter( 'custom_menu_order', 'cfs_p2p_submenu_order' );
 
 /*==================================================
     Add CSS to edit.php
